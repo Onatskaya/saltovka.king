c23b359b418e9f2f100cb39dff9140a6309f4d81
Onatskaya <Onatskaya.Nadejda@gmail.com>
2016-02-22 15:52:56 +0200

    Pages for every couch were created

--- js/init.js
+++ js/init.js
@@ -30,6 +30,8 @@ $(document).ready(function() {
   $('#rulls').width($(document).width());
   /*$('#couches').height($(document).height());
   $('#couches').width($(document).width());*/
+  $('#theCouch').height($(document).height());
+  $('#theCouch').width($(document).width());
 
   /* Цены */
   $( "#prices_accordion" ).accordion({
@@ -44,8 +46,18 @@ $(document).ready(function() {
       active: false
   });
   
+  /* Расписание */
+  $( ".theCouch-schedule" ).accordion({
+      heightStyle: "content",
+      collapsible: true,
+      active: false
+  });
+  
   /* Галерея */
   $('#photo_gallery').photobox('a',{ time:0 });
+
+  /* Фото тренировочного процесса */
+  $('#theCouch .photos').photobox('a',{ time:0 });
 
   /* Слайдер тренеров */
   $('#couches_slider').slick({
@@ -79,40 +91,41 @@ $(document).ready(function() {
   /* Гугл карта */
     google_maps(); // Load google maps onload page
 
-          function google_maps() {
-            var latlng = new google.maps.LatLng(50.032145, 36.356553);
-            var settings = {
-                zoom: 16,
-                center: latlng,
-                mapTypeControl: false,
-                mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-                scrollwheel: false,
-                disableDoubleClickZoom: true,
-                navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("map"), settings);
-            var myLatlng = new google.maps.LatLng(50.032145, 36.356553);
-            
-            var myOptions = {
-                zoom: 16,
-                center: myLatlng,
-                scrollwheel: false,
-                disableDoubleClickZoom: true,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
+    function google_maps() {
+      var latlng = new google.maps.LatLng(50.032145, 36.356553);
+      var settings = {
+          zoom: 16,
+          center: latlng,
+          mapTypeControl: false,
+          mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+          scrollwheel: false,
+          disableDoubleClickZoom: true,
+          navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("map"), settings);
+      var myLatlng = new google.maps.LatLng(50.032145, 36.356553);
+      
+      var myOptions = {
+          zoom: 16,
+          center: myLatlng,
+          scrollwheel: false,
+          disableDoubleClickZoom: true,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-            // Создаем и размещаем маркер на карте
+      // Создаем и размещаем маркер на карте
 
-            var marker = new google.maps.Marker({  
-              position: new google.maps.LatLng(50.032145, 36.356553),  
-              map: map,  
-              title: 'Фитнес-центр King',  
-              clickable: false,  
-              scrollwheel: false,
-              icon: 'marker_icon.png' 
-            }); 
-        };
+      var marker = new google.maps.Marker({  
+        position: new google.maps.LatLng(50.032145, 36.356553),  
+        map: map,  
+        title: 'Фитнес-центр King',  
+        clickable: false,  
+        scrollwheel: false,
+        icon: 'marker_icon.png' 
+      }); 
+  };
 
+  
 
 })
